db5d3162de5f5f0eabb17b651e36712384e87b0b
febbyfeysyah <febbyfeysyah@gmail.com>
2016-03-12 18:51:06 +0700

    Pelamar.java by Febby
    Febby Febriansyah
    1301140371
    IF3801

--- Pelamar.java
+++ Pelamar.java
@@ -1,1 +1,16 @@
 # TugasPBOKelompok9
+public class Pelamar extends Orang{
+	private BerkasLamaran berkas;
+	
+	public void createBerkas(long idBerkas){
+		berkas=new berkas(idBerkas);
+	}
+/* 	public void cariLowongan(String nama,Perusahaan perusahaan){
+		
+	}
+	public void daftarLowongan(){
+		
+	} 
+	
+	Cat: pelamar tidak ber-relasi dengan perusahaan maupun lowongan*/
+}
