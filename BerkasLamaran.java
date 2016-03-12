c77103cc26cdbabc89e6cf308c78ca98ae244499
yuri liadi <yuriliadi@gmail.com>
2016-03-12 18:28:08 +0700

    Update BerkasLamaran.java

--- BerkasLamaran.java
+++ BerkasLamaran.java
@@ -1,1 +1,13 @@
-# TugasPBOKelompok9
+public class BerkasLamaran{
+	private long idBerkas;
+	
+	public BerkasLamaran(long idBerkas){
+		this.idBerkas=idBerkas;
+	}
+	public void setIdBerkas(long idBerkas){
+		this.idBerkas=idBerkas;
+	}
+	public long getIdBerkas(){
+		return idBerkas;
+	}
+}
