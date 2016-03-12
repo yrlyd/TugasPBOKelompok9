dceb1677bca4a5f7d97ab5ba52ffbca867b2e826
yuri liadi <yuriliadi@gmail.com>
2016-03-12 18:27:44 +0700

    Update Orang.java

--- Orang.java
+++ Orang.java
@@ -1,1 +1,38 @@
-# TugasPBOKelompok9
+public class abstract Orang {
+	private long id;
+	private String name;
+	private String email;
+	private long noTlp;
+	
+	public Orang(long id, String name, String email, long noTlp){
+		this.id=id;
+		this.name=name;
+		this.email=email;
+		this.noTlp=noTlp;
+	}
+	public void setId(long id){
+		this.id=id;
+	}
+	public long getId(){
+		return id;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setEmail(String email){
+		this.email=email;
+	}
+	public String getEmail(){
+		return email;
+	}
+	public long getNoTlp(){
+		return noTlp;
+	}
+	public void setNoTlp(long noTlp){
+		this.noTlp=noTlp;
+	}
+}
+
