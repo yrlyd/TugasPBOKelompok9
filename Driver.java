e424fa80e32f4187f5f3e5bc03d9feecf1c8e1f9
yuri liadi <yuriliadi@gmail.com>
2016-03-26 22:12:41 +0700

    Driver.java

--- Driver.java
+++ Driver.java
@@ -1,19 +1,1 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tubes;
-
-/**
- *
- * @author FebbyFebriansyah
- */
-public class Driver {
-
-    public static void main(String[] args) {
-        Aplikasi app = new Aplikasi(2, 2);
-        app.mainMenu();
-    }
-}
 
