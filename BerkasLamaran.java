c26a567f74c1a697a35925676791fe1a4204efcf
febbyfeysyah <febbyfeysyah@gmail.com>
2016-03-26 22:21:35 +0700

    Update BerkasLamaran.java
    FebbyFebriansyah(1301140371)

--- BerkasLamaran.java
+++ BerkasLamaran.java
@@ -1,1 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+/**
+ *
+ * @author FebbyFebriansyah
+ */
+public class BerkasLamaran {
+
+    private long idBerkas;
+    private String namaPelamar;
+
+    public BerkasLamaran(long idBerkas, String namaPelamar) {
+        this.idBerkas = idBerkas;
+        this.namaPelamar = namaPelamar;
+    }
+
+    public BerkasLamaran(long idBerkas) {
+        this.idBerkas = idBerkas;
+    }
+
+    public void setIdBerkas(long idBerkas) {
+        this.idBerkas = idBerkas;
+    }
+
+    public long getIdBerkas() {
+        return idBerkas;
+    }
+
+    public String getNamaPelamar() {
+        return namaPelamar;
+    }
+
+}
 
