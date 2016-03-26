31527be98032f0cf4c331cf8139daef18f39942a
yuri liadi <yuriliadi@gmail.com>
2016-03-26 22:12:21 +0700

    BerkasLamaran.java

--- BerkasLamaran.java
+++ BerkasLamaran.java
@@ -1,13 +1,1 @@
-public class BerkasLamaran{
-	private long idBerkas;
-	
-	public BerkasLamaran(long idBerkas){
-		this.idBerkas=idBerkas;
-	}
-	public void setIdBerkas(long idBerkas){
-		this.idBerkas=idBerkas;
-	}
-	public long getIdBerkas(){
-		return idBerkas;
-	}
-}
+
